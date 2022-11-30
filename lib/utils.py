f495793e4f2901ada711a501fde5f2144d34caba
david dalva <david9916d@gmail.com>
2022-11-30 15:13:17 +0200

    fix

---
+++ lib/utils.py
@@ -0,0 +1,14 @@
+import img2pdf
+from PIL import Image
+import os
+  
+def convert(filename):
+    img_path = filename
+    pdf_path = file.pdf
+    image = Image.open(img_path)
+    pdf_bytes = img2pdf.convert(image.filename)
+    file = open(pdf_path, "wb")
+    file.write(pdf_bytes)
+    image.close()
+    file.close()
+    print("Successfully made pdf file")
--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 from flask import Flask
-
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/convert")
 def hello_world():
     return "<p>Hello, World!</p>"
 
