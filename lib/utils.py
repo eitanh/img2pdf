a90a09b561a2dcd80bafca76ea55d6ba01ab69dc
Eitan Herman <eitan@securegion.com>
2022-12-05 09:39:25 +0200

    fix conf and log

--- lib/utils.py
+++ lib/utils.py
@@ -1,14 +1,22 @@
 import img2pdf
 from PIL import Image
 import os
+import logging
+import yaml
   
 def convert(filename):
-    img_path = filename
-    pdf_path = "file.pdf"
-    image = Image.open(img_path)
-    pdf_bytes = img2pdf.convert(image.filename)
-    file = open(pdf_path, "wb")
-    file.write(pdf_bytes)
-    image.close()
-    file.close()
-    return "Successfully made pdf file"
+    print("start")
+    with open("config.yaml") as config:
+        confdata=yaml.load(config,Loader=yaml.FullLoader)
+        jpgPath=confdata['jpgPath']
+        pdfPath=confdata['pdfPath']
+        logging.info("Converting "+jpgPath+filename+" to "+pdfPath)
+        img_path = jpgPath+filename+".jpg"
+        pdf_path = pdfPath+filename+".pdf"
+        image = Image.open(img_path)
+        pdf_bytes = img2pdf.convert(image.filename)
+        file = open(pdf_path, "wb")
+        file.write(pdf_bytes)
+        image.close()
+        file.close()
+        return "Successfully made pdf file"
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from flask import Flask
 from flask import request
 from lib.utils import convert
+import logging
+
 app = Flask(__name__)
 
 @app.route("/convert")
@@ -8,5 +10,10 @@ def hello_world():
     filename = request.args.get('filename')
     return convert(filename)
 
+@app.route("/test")
+def test():
+   return "Test"
+
+logging.basicConfig(format='%(asctime)s - %(message)s',level=logging.INFO,filename="convert.log")
 if __name__ == '__main__':
     app.run()
